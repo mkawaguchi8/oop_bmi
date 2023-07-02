f9faa6532cf5d91236d8efdfb0cdd114442519ec
mkawaguchi8 <minori22dmo@gmail.com>
2023-07-02 17:19:11 +0900

    アプリ完成

--- bmi.py
+++ bmi.py
@@ -1,17 +1,24 @@
 class BMI:
     def __init__(self, height, weight):
-        self.height = height
-        self.weight = weight
+        # インスタンス変数として計算式の結果をself.valueに代入
+        self.value = weight / (height ** 2)
+        # initメソッドでtanaka_bmiのオブジェクトができる
 
-    def calculate_bmi(self):
-        # bmiの計算式を返す 体重÷身長×身長
-        return self.weight / (self.height ** 2)
+        if not (10 <= self.value <= 40):
+            raise ValueError("BMIが正常値の範囲を超えています")
     
+    def __str__(self):
+        return f"{self.value:.2f}"
+    # オブジェクトをprint()やformat()で呼び出した場合に文字列を返すためのメソッド 返り値はstring(文字列)である必要
 
-# BMIクラスのインスタント化
-tanaka_bmi = BMI(height=1.80, weight=67.0)
-sasami_bmi = BMI(height=1.58, weight=80.0)
+# ここより上はただの設計図
 
-print(tanaka_bmi.calculate_bmi())
-print(sasami_bmi.calculate_bmi())
+# BMIクラスのインスタンス化
+tanaka_bmi = BMI(height=1.80, weight=67.0) # 引数 #メソッドに対して、入れているということ
+sasami_bmi = BMI(height=1.58, weight=80.0) # スペースは不要
+bob_bmi = BMI(height=1.70, weight=75000.0)
 
+print(tanaka_bmi)
+print(sasami_bmi)
+print("error")
+print(bob_bmi)
